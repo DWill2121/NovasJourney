bbaa17cbf02be46c524f883cd074b99fe11f2434
DWill2121 <dereanandre@gmail.com>
2020-01-08 16:25:18 -0500

    Workshop Game

---
+++ NovasJourney/Source/NovasJourney/NovasJourneyGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NovasJourneyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NOVASJOURNEY_API ANovasJourneyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
