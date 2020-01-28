a98e14cc111bbdcc8121d307601158c0cda305d0
Justin <justinrover25@gmail.com>
2020-01-28 16:56:36 -0500

    Nova Fixes
    Camera should expose light right camer controls shoul work

--- NovasJourney/Intermediate/Build/Win64/NovasJourneyEditor/Development/Engine/SharedPCH.Engine.cpp
+++ NovasJourney/Intermediate/Build/Win64/NovasJourneyEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -1,1 +1,1 @@
-#include "C:/Users/de768578/Documents/GitHub/NovasJourney/NovasJourney/Intermediate/Build/Win64/NovasJourneyEditor/Development/Engine/SharedPCH.Engine.h"
+#include "C:/Users/ju151424.NET/Documents/GitHub/NovasJourney/NovasJourney/Intermediate/Build/Win64/NovasJourneyEditor/Development/Engine/SharedPCH.Engine.h"
